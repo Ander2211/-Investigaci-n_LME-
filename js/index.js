a31560dacfbc56b77797602ec7caee2a161c0a58
Ander2211 <anderhdz06@gamil.com>
2024-08-10 10:22:27 -0600

    cambios JS

--- js/index.js
+++ js/index.js
@@ -6,15 +6,13 @@ function calculateBMI() {
         alert('Por favor ingrese peso y altura.');
         return;
     }
-    if(weight >=100 || height >= 220){
-        alert('valores ');
+    if(weight >= 100 || height >= 2.20){
+        alert('Los valores ingresados no son válidos.');
         return;
     }
 
-    var heightInMeters = height / 100;
-    var bmi = weight / (heightInMeters * heightInMeters);
+    
+    var bmi = weight / (height * height);
 
-    var resultText = '';
-
-    document.getElementById('result').innerText = `Tu IMC es ${bmi.toFixed(2)} `;
+    document.getElementById('result').innerText = `Tu IMC es ${bmi.toFixed(2)}`;
 }
